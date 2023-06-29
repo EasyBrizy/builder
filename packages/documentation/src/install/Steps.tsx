d3e1db4261e1affa1bb10bc083a4d534457f11b1
Maxim Valuta <maxval1991@gmail.com>
2023-06-29 11:08:00 +0300

    fix(documentation): steps, added links

--- packages/documentation/src/install/Steps.tsx
+++ packages/documentation/src/install/Steps.tsx
@@ -9,26 +9,22 @@ export const Steps = ({ token }: Props): ReactElement => {
     <div className="flex flex-col flex-[2_2_0%] mx-[15px] md:my-[20px] md:ml-[15px] lg:ml-[75px]">
       <div className="mb-[15px] md:mb-[0px]" />
       <Step
-        title="Step 1: Choose project"
-        content={
-          <div className="text-[17px]">
-            <div>
-              <span className="leading-8">
-                Access the project from <i>Dashboard</i>.
-              </span>
-            </div>
-          </div>
-        }
-      />
-      <div className="mb-[59px]" />
-      <Step
-        title="Step 2: Find the environment variables"
+        title="Step 1: Find the environment variables"
         content={
           <div className="text-[17px] break-all">
             <div>
               <span className="leading-8">
                 In tab <i>Setting</i> access the menu{" "}
-                <i>Environment Variables.</i>
+                <i>
+                  <a
+                    className="text-oceanBlue"
+                    target="_blank"
+                    href="https://vercel.com/docs/concepts/projects/environment-variables"
+                  >
+                    Environment Variables
+                  </a>
+                  .
+                </i>
               </span>
             </div>
           </div>
@@ -36,7 +32,7 @@ export const Steps = ({ token }: Props): ReactElement => {
       />
       <div className="mb-[59px]" />
       <Step
-        title="Step 3: Fill the environment variable"
+        title="Step 2: Fill the environment variable"
         content={
           <div className="text-[17px] break-all">
             <span className="leading-8">
@@ -60,12 +56,33 @@ export const Steps = ({ token }: Props): ReactElement => {
           </div>
         }
       />
-      <div className="mb-[65px]" />
-      <div className="text-[16px] text-russianViolet opacity-60 leading-[1.8rem] tracking-[-.023em]">
-        <em>
-          Note: Please use the following credentials to add Brizy to Vercel.
-        </em>
-      </div>
+      <div className="mb-[59px]" />
+      <Step
+        title="Step 3: Redeploy your project"
+        content={
+          <div className="text-[17px] break-all">
+            <div>
+              <span className="leading-8">
+                <a
+                  className="text-oceanBlue"
+                  href="https://vercel.com/docs/concepts/projects/environment-variables"
+                  target="_blank"
+                >
+                  Redeploy
+                </a>{" "}
+                your project to update the value of these variables in the
+                deployment.
+              </span>
+            </div>
+          </div>
+        }
+      />
+      {/*<div className="mb-[65px]" />*/}
+      {/*<div className="text-[16px] text-russianViolet opacity-60 leading-[1.8rem] tracking-[-.023em]">*/}
+      {/*  <em>*/}
+      {/*    Note: Please use the following credentials to add Brizy to Vercel.*/}
+      {/*  </em>*/}
+      {/*</div>*/}
     </div>
   );
 };
