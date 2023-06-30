c0385ef605f0593480d494ef64f1f403af5e566f
CristianSpatari <spataricristinel@gmail.com>
2023-06-30 14:15:43 +0300

    feat(docs): fix height

--- packages/documentation/src/install/index.tsx
+++ packages/documentation/src/install/index.tsx
@@ -9,7 +9,7 @@ export interface Props {
 
 export const Install = ({ token }: Props): ReactElement => {
   return (
-    <div className="flex items-center justify-center bg-no-repeat bg-cover md:h-[1024px] md:bg-[url('https://a-cloud.b-cdn.net/media/original/20acd60ff6e49607aa60f60eee49bf8b/bg-brizy-builder.jpg')] lg:h-[1335px] lg:items-start lg:pt-[335px]">
+    <div className="flex items-center justify-center bg-no-repeat bg-cover md:h-[100vh] md:bg-[url('https://a-cloud.b-cdn.net/media/original/20acd60ff6e49607aa60f60eee49bf8b/bg-brizy-builder.jpg')]">
       <div className="flex flex-col text-cetaceanBlue font-sans bg-cover bg-no-repeat min-h-[100vh] md:min-h-[0vh] bg-[url('https://a-cloud.b-cdn.net/media/original/20acd60ff6e49607aa60f60eee49bf8b/bg-brizy-builder.jpg')] md:bg-none md:mx-[15px] md:flex-row lg:w-[1180px]">
         <Information title={"Documentation"} />
         <Divider />
