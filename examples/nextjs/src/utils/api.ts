ac4ce96653c8a4981fc93ca88789d7f3aa622859
Maxim Valuta <maxval1991@gmail.com>
2023-07-11 14:06:52 +0300

    feat(systemPage): added systemPages

--- examples/nextjs/src/utils/api.ts
+++ examples/nextjs/src/utils/api.ts
@@ -2,6 +2,39 @@ import { Client, CompilerData } from "@brizy/react";
 import Config from "@config";
 
 //#region API
+interface SystemPage {
+  comingSoon?: string;
+  maintenance?: string;
+  pageNotFound?: string;
+  protectedProject?: {
+    slug: string;
+    password: string;
+  };
+  resetPasswordPage?: string;
+}
+interface BuilderConfig {
+  systemPage?: SystemPage;
+}
+
+const systemPage: SystemPage = {
+  // Page shown while site is in development
+  comingSoon: "", // /coming-soon
+
+  // Page shown while down for maintenance
+  maintenance: "", // /maintenance
+
+  // Used to show missing links
+  pageNotFound: "", // /page-not-found
+
+  // Password-protected page:
+  protectedProject: {
+    slug: "/protected-page",
+    password: "9a66a9d073d56cead9ceb03d79dfad4e",
+  },
+
+  // Used to retrieve account password
+  resetPasswordPage: "",
+};
 
 interface APIData {
   collection: "page";
@@ -26,6 +59,12 @@ class API {
 
   public getHTMLByItem({ item }: APIData): Promise<CompilerData> {
     return this.client.page.getPages(item);
+  }
+
+  public getConfig(): Promise<BuilderConfig> {
+    return new Promise((res) => {
+      res({ systemPage });
+    });
   }
 }
 
