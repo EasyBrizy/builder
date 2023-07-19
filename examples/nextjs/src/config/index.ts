ef3c5367297d1ab336165c896a0e8563613079e6
oxigos <124198041+oxigos@users.noreply.github.com>
2023-07-19 11:31:43 +0300

    Update index.ts

--- examples/nextjs/src/config/index.ts
+++ examples/nextjs/src/config/index.ts
@@ -2,7 +2,7 @@ const builderHost = "https://brizy.cloud";
 
 const Config = {
   apiKey: process.env["API_KEY"] ?? "",
-  builderWelcomeUrl: `${builderHost}/share/TGh6Xkll111EfZd`,
+  builderWelcomeUrl: `${builderHost}/share/gBJSKbID2OGrDY7`,
 };
 
 export default Config;
